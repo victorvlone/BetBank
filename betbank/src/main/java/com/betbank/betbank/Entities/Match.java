b2c048c31a613dc47665a027e619d62a65228f63
João Victor <victorvlone999@gmail.com>
2025-11-25 16:26:06 -0300

    feat: connection made to database

--- betbank/src/main/java/com/betbank/betbank/Entities/Match.java
+++ betbank/src/main/java/com/betbank/betbank/Entities/Match.java
@@ -1,9 +1,15 @@
 package com.betbank.betbank.Entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,19 +19,41 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "match")
+@Entity
+@Table(name = "matches")
 public class Match {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne 
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "home", length = 100, nullable = false)
     private String home;
+
+    @Column(name = "away", length = 100, nullable = false)
     private String away;
+
+    @Column(name = "homeWin", nullable = false)
     private Integer homeWin;
+
+    @Column(name = "over15", nullable = false)
     private Integer over15;
+
+    @Column(name = "over25", nullable = false)
     private Integer over25;
+
+    @Column(name = "over05Ht", nullable = false)
     private Integer over05Ht;
+
+    @Column(name = "btts", nullable = false)
     private Integer btts;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "result")
     private Result result;
 
 }
--- betbank/src/main/java/com/betbank/betbank/Entities/User.java
+++ betbank/src/main/java/com/betbank/betbank/Entities/User.java
@@ -3,10 +3,13 @@ package com.betbank.betbank.Entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,14 +19,31 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "user")
+@Entity
+@Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
     public String name;
+
+    @Column(name = "email", length = 150, nullable = false)
     public String email;
+
+    @Column(name = "password", length = 50, nullable = false)
     public String password;
+
+    @OneToMany(mappedBy = "user")
     private List<Match> matchs = new ArrayList<>();
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
 }
